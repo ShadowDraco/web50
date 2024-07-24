8d07b572e28607c24d92862da026be058ba2ae09
Ethan Storm <github.balsamic226@passinbox.com>
2024-07-23 17:54:35 -0700

    finish lecture

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Flight, Passenger
 
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    })
+
+def book(request, flight_id):
+    if request.method == 'POST':
+        # Get requested flight
+        flight = Flight.objects.get(pk=flight_id)
+        # Get requested passenger
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        # Add add passenger to related flights table
+        passenger.flights.add(flight)
+
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
