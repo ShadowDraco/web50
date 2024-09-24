168e6a5b6e4d73205ae97370d0a551b9a4a7bf94
Ethan Storm <github.balsamic226@passinbox.com>
2024-09-24 16:12:02 -0700

    examples of ways to test

---
+++ prime.py
@@ -0,0 +1,12 @@
+import math 
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(math.sqrt(n))):
+        if n % i == 0:
+            return False
+    return True
+
+assert(is_prime(1)) == False
+assert(is_prime(5)) == True
